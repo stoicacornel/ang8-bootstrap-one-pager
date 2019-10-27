2d76edcd2d3bb2207b5a2f8c3c8a63bcdc568284
Cornel <StoicaCornel749@yahoo.com>
2019-10-27 13:53:38 +0000

    Added scroll to sections and to top.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,29 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.scroll(
+        'app-down-arrow,  #top_bar_scroll_to_about, #top_bar_scroll_to_venue_placeholder',
+        'app-about-content'
+    );
+
+    this.scroll(
+        '#top_bar_scroll_to_speaker',
+        'app-speaker-content'
+    );
+
+    this.scroll(
+        '#footer_copyright, #footer_notice, #footer_privacy, #footer_sitemap',
+        '#landing',
+        1000
+    );
   }
 
+  scroll(from, to, duration = 600) {
+    $(from).on( 'click', () => {
+      $('html, body').animate({
+            scrollTop: $(to).offset().top
+          }, duration
+      );
+    });
+  }
 }
