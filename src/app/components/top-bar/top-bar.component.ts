3cd5a0328a286c7347eff841e172140d4696a544
Cornel <StoicaCornel749@yahoo.com>
2019-10-27 21:39:47 +0000

    Responsive Top bar (including hamburger menu with animation) and footer done.

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent {
 
   constructor() { }
 
-  ngOnInit() {
+  hamburgerToggle() {
+    document.getElementById("menu").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
   }
-
 }
